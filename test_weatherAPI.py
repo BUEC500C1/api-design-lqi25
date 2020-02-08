9f7afbc2c105ff0272109d2c9af101183a8153c1
lqi25 <55333782+lqi25@users.noreply.github.com>
2020-02-08 13:47:37 -0500

    Update test_weatherAPI.py

--- test_weatherAPI.py
+++ test_weatherAPI.py
@@ -5,10 +5,9 @@ Created on Sat Feb  8 13:27:34 2020
 @author: 18367
 """
 import pytest
-import requests
-from weather import get_city_weather,forecast_airport_weather,get_airport_weather,current_weather
+from weather import get_city_weather,forecast_airport_weather,get_airport_weather,current_weather,requests
 
 def test_weather():
-    assert get_city_weather("Boston")
-    assert get_airport_weather("Total Rf Heliport")
-    assert forecast_airport_weather("Total Rf Heliport")
+    get_city_weather("Boston")
+    get_airport_weather("Total Rf Heliport")
+    forecast_airport_weather("Total Rf Heliport")
